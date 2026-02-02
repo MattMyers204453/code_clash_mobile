06e1e267e35719a7bb0412ee239ad0debf9d4d0d
joshjmyers <josh.justin.myers@gmail.com>
2026-02-01 17:49:41 -0700

    added buttons

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,25 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
+        body: const Center(
           child: Text('Hello World!'),
+        ),
+        bottomNavigationBar: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                onPressed: () {print('Creating Game...');},
+                child: const Text('Create Game'),
+              ),
+               ElevatedButton(
+                onPressed: () {print('Joining Game...');},
+                child: const Text('Join Game'),
+              ),
+            ],) 
         ),
       ),
     );
