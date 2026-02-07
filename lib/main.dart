3bb44aed063b6700df7962c562690e7903f5c407
joshjmyers <josh.justin.myers@gmail.com>
2026-02-06 18:37:58 -0700

    add logo to intro page; styled buttons; background black;

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 
 void main() {
   runApp(const MainApp());
@@ -11,23 +13,47 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 0, 0, 0),
         body: Center(
-          child: Text('This is Bens branch'),
+          child: Image.asset(
+            'assets/images/code_clash_logo.jfif',
+            width: 300,
+            height: 300,
+          ),
         ),
+
         bottomNavigationBar: Padding(
           padding: const EdgeInsets.all(16),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               ElevatedButton(
-                onPressed: () {print('Creating Game...');},
-                child: const Text('Create Game'),
+                onPressed: () {
+                  print('Creating Game...');
+                },
+                child: const Text(
+                  'Create Game',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xFF4169E1),
+                  foregroundColor: Colors.white,
+                  minimumSize: Size(175, 50),
+                ),
               ),
-               ElevatedButton(
-                onPressed: () {print('Joining Game...');},
+              ElevatedButton(
+                onPressed: () {
+                  print('Joining Game...');
+                },
                 child: const Text('Join Game'),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xFF4169E1),
+                  foregroundColor: Colors.white,
+                  minimumSize: Size(175, 50),
+                ),
               ),
-            ],) 
+            ],
+          ),
         ),
       ),
     );
